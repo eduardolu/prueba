80a4940f406f1ef5fc6a0a1d530fb2802e201829
eduardolu <edu291086@gmail.com>
2019-09-21 11:46:28 +0200

    subir

--- Server-Sonia/src/dataBase.ts
+++ Server-Sonia/src/dataBase.ts
@@ -4,11 +4,10 @@ import { PoolCluster } from 'mysql';
 
 const pool = mysql.createPool(keys.database);
 
-
-
-pool.getConnection().then(connection => {
-    pool.releaseConnection(connection);
-    console.log('BD is connection');
-});
+pool.getConnection()
+    .then(connection => {
+        pool.releaseConnection(connection);
+        console.log('DB is Connected');
+    });
 
 export default pool;
